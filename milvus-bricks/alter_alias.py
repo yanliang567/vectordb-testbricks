bc7add89e0d2019e8acff5eeafc82e6db88f0be5
yanliang567 <yanliang.qiao@zilliz.com>
2024-04-28 11:06:51 +0800

    Add load replicas
    Signed-off-by: yanliang567 <yanliang.qiao@zilliz.com>

--- milvus-bricks/alter_alias.py
+++ milvus-bricks/alter_alias.py
@@ -17,7 +17,8 @@ if __name__ == '__main__':
     metric_type = str(sys.argv[4]).upper()                  # metric type is L2 or IP
     need_insert = str(sys.argv[5]).upper()                  # need insert data or not
     is_flush = str(sys.argv[6]).upper()                     # flush data or not
-    drop_after_alter_alias = str(sys.argv[7]).upper()       # drop collection after alter alias or not
+    load_replicas = int(sys.argv[7])                        # load replicas number
+    drop_after_alter_alias = str(sys.argv[8]).upper()       # drop collection after alter alias or not
 
     alias_name = f"{c_name}_alias"                  # alias mame
     port = 19530
@@ -35,6 +36,7 @@ if __name__ == '__main__':
     insert_times = 50 if need_insert else 0
     drop_after_alter_alias = True if drop_after_alter_alias == "TRUE" else False
     is_flush = True if is_flush == "TRUE" else False
+    load_replicas = 1 if load_replicas < 1 else load_replicas
     # check and get the collection info
     exists_aa = utility.has_collection(collection_name=f'{c_name}_aa')
     exists_bb = utility.has_collection(collection_name=f'{c_name}_bb')
@@ -58,7 +60,7 @@ if __name__ == '__main__':
             logging.info(f"collection alias before altered: {description}, num_entities: {current_collection.num_entities}")
             next_name = f"{c_name}_bb" if description == f"{c_name}_aa" else f"{c_name}_aa"
             next_collection = Collection(next_name)
-            next_collection.load()
+            next_collection.load(replica_number=load_replicas)
             utility.alter_alias(collection_name=next_name, alias=alias_name)
             current_collection = Collection(description)
             current_collection.release()
@@ -67,12 +69,12 @@ if __name__ == '__main__':
         else:
             next_name = f"{c_name}_aa"
             next_collection = Collection(next_name)
-            next_collection.load()
+            next_collection.load(replica_number=load_replicas)
             utility.create_alias(collection_name=next_name, alias=alias_name)
     except pymilvus.exceptions.DescribeCollectionException as e:
         next_name = f"{c_name}_aa"
         next_collection = Collection(next_name)
-        next_collection.load()
+        next_collection.load(replica_number=load_replicas)
         utility.create_alias(collection_name=next_name, alias=alias_name)
     collection = Collection(alias_name)
     logging.info(f"collection alias after altered: {collection.description}, num_entities: {collection.num_entities}")
