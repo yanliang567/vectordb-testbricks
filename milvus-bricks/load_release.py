90ff7a8506550b3f8cf4718f7be2603e5fac0297
yanliang567 <yanliang.qiao@zilliz.com>
2024-05-06 18:53:12 +0800

    Add load release test
    Signed-off-by: yanliang567 <yanliang.qiao@zilliz.com>

--- milvus-bricks/load_release.py
+++ milvus-bricks/load_release.py
@@ -15,33 +15,17 @@ DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"
 if __name__ == '__main__':
     host = sys.argv[1]
     name = sys.argv[2]                              # collection mame/alias
-    timeout = int(sys.argv[3])                      # search timeout, permanently if 0
-    output_fields = str(sys.argv[4]).strip()        # output fields, default is None
-    expr = str(sys.argv[5]).strip()                 # search expression, default is None
-    nq = int(sys.argv[6])                           # search nq
-    topk = int(sys.argv[7])                         # search topk
-    reload = str(sys.argv[8]).upper()               # reload collection before search
-    api_key = str(sys.argv[9])                      # api key for cloud instances
+    reload_times = int(sys.argv[3])                 # reload times
+    api_key = str(sys.argv[4])                      # api key for cloud instances
     port = 19530
 
-    if output_fields.upper() == "NONE" or output_fields == "":
-        output_fields = None
-    else:
-        output_fields = output_fields.split(",")
-    if expr.upper() == "NONE" or expr == "":
-        expr = None
-
-    reload = True if reload == "TRUE" else False
-
-    file_handler = logging.FileHandler(filename=f"/tmp/cold_search_{name}.log")
+    file_handler = logging.FileHandler(filename=f"/tmp/load_release_{name}.log")
     stdout_handler = logging.StreamHandler(stream=sys.stdout)
     handlers = [file_handler, stdout_handler]
     logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT, datefmt=DATE_FORMAT, handlers=handlers)
     logger = logging.getLogger('LOGGER_NAME')
 
-    logging.info(f"searching collection={name}, host={host}, timeout={timeout}, "
-                 f"output_fields={output_fields}, expr={expr}, nq={nq}, "
-                 f"topk={topk}, reload={reload}, api_key={api_key}")
+    logging.info(f"searching collection={name}, host={host}, reload_times={reload_times} api_key={api_key}")
 
     if api_key is None or api_key == "" or api_key.upper() == "NONE":
         conn = connections.connect('default', host=host, port=port)
@@ -64,48 +48,39 @@ if __name__ == '__main__':
         exit(-1)
 
     index_params = get_index_params(collection)
-    search_params = get_search_params(collection, topk)
     logging.info(f"index param: {index_params}")
-    logging.info(f"search_param: {search_params}")
-    logging.info(f"output_fields: {output_fields}")
-    logging.info(f"expr: {expr}")
 
     # flush before indexing
     num = collection.num_entities
     logging.info(f"{name} num_entities: {num}")
     logging.info(f"{name} index progress: {utility.index_building_progress(name)}")
 
-    # release collection
-    if reload:
+    for i in range(reload_times):
+        # release collection
         t1 = time.time()
         collection.release()
         t2 = round(time.time() - t1, 3)
         logging.info(f"assert release {name}: {t2}")
-        time.sleep(5)
 
+        time.sleep(120)
         # load collection
         t1 = time.time()
         collection.load()
         t2 = round(time.time() - t1, 3)
         logging.info(f"assert re-load {name}: {t2}")
 
-    logging.info(f"cold search start: nq{nq}_top{topk}")
-    dim = get_dim(collection)
-    vector_field_name = get_vector_field_name(collection)
-    start_time = time.time()
-    i = 0
-    while time.time() < start_time + timeout:
+        dim = get_dim(collection)
+        vector_field_name = get_vector_field_name(collection)
+        nq = 1
+        topk = 10
         search_vectors = [[random.random() for _ in range(dim)] for _ in range(nq)]
-        parkey = random.randint(1, 1000)
-        # exact_expr = "category==27"
-        exact_expr = None if expr is None else expr + str(parkey)
+        search_params = get_search_params(collection, topk)
         t1 = time.time()
         try:
-            collection.search(data=search_vectors, anns_field=vector_field_name, output_fields=output_fields, expr=exact_expr, param=search_params, limit=topk)
+            collection.search(data=search_vectors, anns_field=vector_field_name, param=search_params, limit=topk)
         except Exception as e:
             logging.error(e)
         t2 = round(time.time() - t1, 4)
-        i += 1
-        logging.info(f"collection {collection.description} {i} search: cost {t2}")
+        logging.info(f"collection {collection.description} search: cost {t2}")
 
-    logging.info(f"cold search completed")
+    logging.info(f"load release for {reload_times} times completed")
